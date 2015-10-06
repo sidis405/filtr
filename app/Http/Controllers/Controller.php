b71c9fadfc376371c87b0a5e00c449b666fb0e84
Sidrit Trandafili <forge405@gmail.com>
2015-10-06 17:03:45 +0200

    changes sid - filtr.dev Tue Oct  6 17:03:45 CEST 2015

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -17,7 +17,7 @@ abstract class Controller extends BaseController
     public function __construct() {
         $this->user = $this->isSignedIn = \Auth::user();
         view()->share('user', $this->user);
-        view()->share('title', $title);
+        view()->share('title', 'Filtr');
         view()->share('isSignedIn', $this->isSignedIn);
     }
 }
