765dffe47e24b6cac9f991f119016597ae288ea0
Sidrit Trandafili <forge405@gmail.com>
2015-10-06 16:40:41 +0200

    changes sid - filtr.dev Tue Oct  6 16:40:41 CEST 2015

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,7 +9,8 @@
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Rock+Salt|Playball|Roboto' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="/css/main.css">
-    
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">    
             
     <meta name="csrf_token" content="{{csrf_token()}}">
 </head>
